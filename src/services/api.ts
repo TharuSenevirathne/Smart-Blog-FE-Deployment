071125c0c10f6db6b075330918717e8749a92f3b
tharusenevirathne <tharusanduni33@gmail.com>
2025-11-24 11:42:23 +0530

    update

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,8 @@ import axios, { AxiosError } from "axios";
 import { refreshTokens } from "./auth";
 
 const api = axios.create({
-    baseURL:'http://localhost:5000/api/v1'
+    // baseURL:'http://localhost:5000/api/v1'
+    baseURL:'https://smart-blog-be-deployment.vercel.app/api/v1'
 })
 
 const PUBLIC_ENDPOINTS = [
